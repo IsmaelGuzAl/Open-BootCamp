02c8dfd1003cc270412fd3309d9b4b3895e779d3
Ismael <109512279+IsmaelGuzAl@users.noreply.github.com>
2022-12-29 12:31:49 +0100

    JS

---
+++ JavaScrip Basico/02-Sintaxis/sintaxis.js
@@ -0,0 +1,12 @@
+const lista = [
+  "Ismael",
+  38,
+  true,
+  new Date(1984, 9, 13),
+  { 
+      titulo: "El señor de los anillos",
+      autor: "J.R.R. Tolkien",
+      fecha: new Date(1954, 6, 29),
+      url: "https://www.amazon.es/Anillos-Comunidad-Anillo-Biblioteca-Tolkien/dp/8445000667"
+    },
+];
